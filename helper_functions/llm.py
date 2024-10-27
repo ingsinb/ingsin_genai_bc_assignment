be1467c65072fa935a5074a73288eede02655e79
ingsinb <ingsin96@gmail.com>
2024-10-28 00:05:58 +0800

    Update llm.py

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -8,8 +8,22 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 
-load_dotenv('.env')
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+import os
+import streamlit as st
+from dotenv import load_dotenv
+from openai import OpenAI
+import tiktoken
+
+
+if load_dotenv('.env'):
+   # for local development
+   OPENAI_KEY = os.getenv('OPENAI_API_KEY')
+else:
+   OPENAI_KEY = st.secrets['OPENAI_API_KEY']
+
+
+# Pass the API Key to the OpenAI Client
+client = OpenAI(api_key=OPENAI_KEY)
 
 # generate embedding
 def get_embedding(input, model='text-embedding-3-small'):
